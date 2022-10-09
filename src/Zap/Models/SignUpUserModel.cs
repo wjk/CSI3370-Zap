a05d03812843d779d45fd67c64c33d24cb31e5c5
William Kent <wjk011@gmail.com>
2022-10-09 16:08:43 -0400

    Mark properties as nullable

--- src/Zap/Models/SignUpUserModel.cs
+++ src/Zap/Models/SignUpUserModel.cs
@@ -10,25 +10,25 @@ namespace Zap.Models
     {
         [Required(ErrorMessage = "Please enter your first name")]
         [Display(Name = "First Name:")]
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
 
         [Display(Name = "Last Name:")]
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
 
         [Required(ErrorMessage = "Please enter your email")]
         [Display(Name = "Email Address:")]
         [EmailAddress(ErrorMessage = "Please enter a valid email")]
-        public string Email {get; set;}
+        public string? Email {get; set;}
 
         [Required(ErrorMessage = "Please enter a strong password")]
         [Compare("ConfirmPassword", ErrorMessage = "Password does not match")]
         [Display(Name = "Password:")]
         [DataType(DataType.Password)]
-        public string Password {get; set;}
+        public string? Password {get; set;}
 
         [Required(ErrorMessage = "Please confirm your password")]
         [Display(Name = "Confirm Password:")]
         [DataType(DataType.Password)]
-        public string ConfirmPassword {get; set;}
+        public string? ConfirmPassword {get; set;}
     }
 }
