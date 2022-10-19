f8757ebea555d3e1d3e41c4bd9bf6f1f46028bf0
theShookman <114439261+theShookman@users.noreply.github.com>
2022-10-18 20:55:20 -0400

    Add files via upload

---
+++ src/Zap/Models/SignInModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zap.Models
+{
+    public class SignInModel
+    {
+        [Required(ErrorMessage = "Please enter your email")]
+        [Display(Name = "Email Address:")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email")]
+        public string? Email {get; set;}
+        
+        [Required(ErrorMessage = "Please enter a password")]
+        [Display(Name = "Password:")]
+        [DataType(DataType.Password)]
+        public string? Password {get; set;}
+    }
+}
