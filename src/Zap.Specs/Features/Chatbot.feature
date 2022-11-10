deed238f5e8d4100fe571af70516077ff2c8cbd9
William Kent <wjk011@gmail.com>
2022-11-10 09:23:47 -0500

    Add Chatbot.feature and its step definitions

---
+++ src/Zap.Specs/Features/Chatbot.feature
@@ -0,0 +1,23 @@
+Feature: Chatbot
+
+Scenario: Kroger food pick-up interaction
+    Given the default test user exists
+    And the default test user is logged in
+    When the chatbot is loaded
+    And action "Food" is selected
+    And action "Groceries" is selected
+    And action "Kroger" is selected
+    And action "PickUp" is selected
+    Then the prompt text should be "Sign in or make an account at Kroger.com. During checkout, select pick-up in store during checkout after you have all the items that you want in your cart."
+    And the external link "Kroger.com" exists
+    
+Scenario: Walmart food delivery interaction
+    Given the default test user exists
+    And the default test user is logged in
+    When the chatbot is loaded
+    And action "Food" is selected
+    And action "Groceries" is selected
+    And action "Walmart" is selected
+    And action "Delivery" is selected
+    Then the prompt text should be "Sign in or make an account at Walmart.com. During checkout, select delivery during checkout after you have all the items that you want in your cart. Be sure to select a good time and date for delivery."
+    And the external link "Walmart.com" exists
---
+++ src/Zap.Specs/Steps/ChatbotStepDefinitions.cs
@@ -0,0 +1,76 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+using Xunit;
+
+using Zap.Specs.Drivers;
+using Zap.Specs.PageObjects;
+
+namespace Zap.Specs.Steps;
+
+[Binding]
+public class ChatbotStepDefinitions
+{
+    private readonly IWebDriver _webDriver;
+    private ChatbotPageObject? _pageObject;
+
+    public ChatbotStepDefinitions()
+    {
+        _webDriver = BrowserDriver.CurrentDriver.CreateWebDriver();
+    }
+
+    [Given("the default test user is logged in")]
+    public void GivenTheDefaultTestUserIsLoggedIn()
+    {
+        _webDriver.Url = "https://localhost:5001/Account/SignIn";
+        WebDriverWait wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(5));
+        wait.UntilAllElementsAppear("emailEntry", "passwordEntry", "submit");
+
+        var element = _webDriver.FindElement(By.Id("emailEntry"));
+        element.Clear();
+        element.SendKeys("testuser@example.com");
+
+        element = _webDriver.FindElement(By.Id("passwordEntry"));
+        element.Clear();
+        element.SendKeys("testuser");
+
+        element = _webDriver.FindElement(By.Id("submit"));
+        element.Click();
+        
+        wait.UntilAllElementsAppear("login-friendly");
+    }
+
+    [When("the chatbot is loaded")]
+    public void WhenTheChatbotIsLoaded()
+    {
+        _pageObject = new ChatbotPageObject(_webDriver);
+        _pageObject.Initialize();
+ 
+        WebDriverWait wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(5));
+        wait.UntilAllElementsAppear("chatbot");
+    }
+
+    [When("action \"(.+)\" is selected")]
+    public void WhenActionIsSelected(string identifier)
+    {
+        var element = _webDriver.FindElement(By.Id("action-" + identifier));
+        element.Click();
+
+        WebDriverWait wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(5));
+        wait.UntilAllElementsAppear("chatbot");
+    }
+
+    [Then("the prompt text should be \"(.+)\"")]
+    public void ThenThePromptTextShouldBe(string text)
+    {
+        ArgumentNullException.ThrowIfNull(_pageObject);
+        Assert.Equal(text, _pageObject.PromptText);
+    }
+
+    [Then("the external link \"(.+)\" exists")]
+    public void ThenTheExternalLinkExists(string text)
+    {
+        ArgumentNullException.ThrowIfNull(_pageObject);
+        Assert.Contains(text, _pageObject.ActionButtonTitles);
+    }
+}
